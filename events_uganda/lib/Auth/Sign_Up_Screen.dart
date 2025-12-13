e4f726a5dee3ed7a5012a676e7005faab5e931fd
Alvin11David <alvin69david@gmail.com>
2025-12-13 07:36:01 +0300

    Added the arrow

--- events_uganda/lib/Auth/Sign_Up_Screen.dart
+++ events_uganda/lib/Auth/Sign_Up_Screen.dart
@@ -30,7 +30,25 @@ class SignUpScreen extends StatelessWidget {
               fit: BoxFit.contain,
             ),
           ),
-          // ...other widgets (e.g., your blurred circle)
+          Positioned(
+            top: MediaQuery.of(context).size.height * 0.04,
+            left: MediaQuery.of(context).size.width * 0.04,
+            child: Container(
+              width: MediaQuery.of(context).size.width * 0.18,
+              height: MediaQuery.of(context).size.width * 0.18,
+              decoration: BoxDecoration(
+                color: const Color(0xFFCB9FE4),
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: Center(
+                child: Icon(
+                  Icons.chevron_left,
+                  color: Colors.black,
+                  size: MediaQuery.of(context).size.width * 0.10,
+                )
+              )
+            )
+          )
         ],
       ),
     );
