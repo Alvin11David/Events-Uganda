6f391d1d59f603ed85c8810e43b32d7fa3e54b37
Tagoole Mugwa David <tagooledavid4@gmail.com>
2025-12-13 13:14:05 +0300

    Not yet started coding

--- events_uganda/lib/Other Screens/Splash_Screen.dart
+++ events_uganda/lib/Other Screens/Splash_Screen.dart
@@ -7,6 +7,11 @@ import 'dart:math' as math;
 class DiagonalLogoText extends StatelessWidget {
   const DiagonalLogoText({super.key});
 
+    @override
+    Widget build(BuildContext context) {
+      final screenWidth = MediaQuery.of(context).size.width;
+      final screenHeight = MediaQuery.of(context).size.height;
+
   @override
   Widget build(BuildContext context) {
     final double screenWidth = MediaQuery.of(context).size.width;
