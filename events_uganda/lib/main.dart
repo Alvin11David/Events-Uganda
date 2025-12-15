8869a30c782b290435f24fac958db6264c15a466
Alvin11David <alvin69david@gmail.com>
2025-12-15 22:34:54 +0300

    Auto commit after delay

--- events_uganda/lib/main.dart
+++ events_uganda/lib/main.dart
@@ -3,12 +3,15 @@ import 'package:events_uganda/Intro/Onboarding_Screen1.dart';
 import 'package:events_uganda/Intro/Onboarding_Screen2.dart';
 import 'package:events_uganda/Intro/Onboarding_Screen3.dart';
 import 'package:events_uganda/Other%20Screens/Splash_Screen.dart';
+import 'package:events_uganda/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
