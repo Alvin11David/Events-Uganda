1edb083f5634aaf71929437cbc59996a22b587c7
Alvin11David <alvin69david@gmail.com>
2025-12-17 14:19:21 +0300

    Added a the search bar

--- events_uganda/lib/Users/Customers/Customer_Home_Screen.dart
+++ events_uganda/lib/Users/Customers/Customer_Home_Screen.dart
@@ -1,8 +1,5 @@
 import 'dart:ui';
-import 'package:events_uganda/Auth/Sign_In_Screen.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:google_sign_in/google_sign_in.dart';
 
 class CustomerHomeScreen extends StatefulWidget {
   const CustomerHomeScreen({super.key});
@@ -13,7 +10,6 @@ class CustomerHomeScreen extends StatefulWidget {
 
 class _CustomerHomeScreenState extends State<CustomerHomeScreen>
     with SingleTickerProviderStateMixin {
-  
   @override
   Widget build(BuildContext context) {
     final screenWidth = MediaQuery.of(context).size.width;
@@ -22,82 +18,147 @@ class _CustomerHomeScreenState extends State<CustomerHomeScreen>
       backgroundColor: Colors.white,
       body: SafeArea(
         child: Stack(
-        children: [
-          Positioned(
-            top: MediaQuery.of(context).size.height * 0.0,
-            bottom: MediaQuery.of(context).size.height * 0.0,
-            right:
-                (MediaQuery.of(context).size.width +
-                    MediaQuery.of(context).size.width * 1) /
-                300,
-            child: Image.asset(
-              'assets/backgroundcolors/normalscreen.png',
-              width: MediaQuery.of(context).size.width * 1.08,
-              height: MediaQuery.of(context).size.height * 0.9,
-              fit: BoxFit.contain,
+          children: [
+            Positioned(
+              top: MediaQuery.of(context).size.height * 0.0,
+              bottom: MediaQuery.of(context).size.height * 0.0,
+              right:
+                  (MediaQuery.of(context).size.width +
+                      MediaQuery.of(context).size.width * 1) /
+                  300,
+              child: Image.asset(
+                'assets/backgroundcolors/normalscreen.png',
+                width: MediaQuery.of(context).size.width * 1.08,
+                height: MediaQuery.of(context).size.height * 0.9,
+                fit: BoxFit.contain,
+              ),
             ),
-          ),
-          Positioned(
-            top: screenHeight * 0.04,
-            left: screenWidth * 0.04,
-            child: Container(
-              width: screenWidth * 0.128,
-              height: screenWidth * 0.128,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                shape: BoxShape.circle,
-                border: Border.all(
+            Positioned(
+              top: screenHeight * 0.04,
+              left: screenWidth * 0.04,
+              child: Container(
+                width: screenWidth * 0.128,
+                height: screenWidth * 0.128,
+                decoration: BoxDecoration(
                   color: Colors.white,
-                  width: 3,
+                  shape: BoxShape.circle,
+                  border: Border.all(color: Colors.white, width: 3),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.15),
+                      blurRadius: 10,
+                      offset: const Offset(0, 7),
+                    ),
+                  ],
                 ),
+                child: Center(
+                  child: Icon(
+                    Icons.menu_open,
+                    color: Colors.black,
+                    size: screenWidth * 0.07,
+                  ),
+                ),
+              ),
+            ),
+            Positioned(
+              top: screenHeight * 0.04,
+              right: screenWidth * 0.2,
+              child: Container(
+                width: screenWidth * 0.128,
+                height: screenWidth * 0.128,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  shape: BoxShape.circle,
+                  border: Border.all(color: Colors.white, width: 3),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.15),
+                      blurRadius: 10,
+                      spreadRadius: 2,
+                      offset: const Offset(0, 7),
+                    ),
+                  ],
+                ),
+                child: Center(
+                  child: Icon(
+                    Icons.person,
+                    color: Colors.black,
+                    size: screenWidth * 0.07,
+                  ),
+                ),
+              ),
+            ),
+            Positioned(
+              top: screenHeight * 0.04,
+              right: screenWidth * 0.04,
+              child: Container(
+                width: screenWidth * 0.128,
+                height: screenWidth * 0.128,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  shape: BoxShape.circle,
+                  border: Border.all(color: Colors.white, width: 3),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.15),
+                      blurRadius: 10,
+                      offset: const Offset(0, 7),
+                    ),
+                  ],
+                ),
+                child: Center(
+                  child: Icon(
+                    Icons.notifications_none_rounded,
+                    color: Colors.black,
+                    size: screenWidth * 0.07,
+                  ),
+                ),
+              ),
+            ),
+            Positioned(
+              top: screenHeight * 0.15,
+              left: screenWidth * 0.04,
+              right: screenWidth * 0.2,
+              child: Container(
+                height: screenWidth * 0.12,
+                decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(30),
                 boxShadow: [
                   BoxShadow(
-                    color: Colors.black.withOpacity(0.15),
-                    blurRadius: 10,
-                    offset: const Offset(0,7),
-                    )      
+                    color: Colors.black.withOpacity(0.1),
+                    blurRadius: 12,
+                    spreadRadius: 2,
+                    offset: const Offset(2,7),
+                  ),
                 ],
-              ),
-              child: Center(
-                child: Icon(
-                  Icons.menu_open,
-                  color: Colors.black,
-                  size: screenWidth * 0.07,
                 ),
-              ),
+                child: Row(
+                  children: [
+                    Padding(
+                    padding: EdgeInsets.only(left: screenWidth * 0.04),
+                    child: Icon(
+                      Icons.search,
+                      color: Colors.black,
+                      size: screenWidth * 0.06,
+                    ),
+                    ),
+                    SizedBox(width: screenWidth * 0.03),
+                    Expanded(
+                      child: Text(
+                        'Search for services, vendors etc...',
+                        style: TextStyle(
+                          color: Colors.black.withOpacity(0.5),
+                          fontSize: screenWidth * 0.033,
+                          fontFamily: 'Montserrat',
+                        )
+                      ),
+                      )
+                  ],
+                ),
+              )
             )
-          ),
-          Positioned(
-            top: screenHeight * 0.04,
-            right: screenWidth * 0.04,
-            child: Container(
-              width: screenWidth * 0.128,
-              height: screenWidth * 0.128,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                shape: BoxShape.circle,
-                border: Border.all(
-                  color: Colors.white,
-                  width: 3,
-                ),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black.withOpacity(0.15),
-                    blurRadius: 10,
-                    offset: const Offset(0,7),
-                    )      
-                ],
-              ),
-              child: Center(
-                child: Icon(
-                  Icons.notifications_none_rounded,
-                  color: Colors.black,
-                  size: screenWidth * 0.07,
-                ),
-              ),
-            )
-          ),
-        ],
+          ],
         ),
       ),
     );
