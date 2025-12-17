0aed22cf4530070e7cf318e604e955ad3d476de4
Alvin11David <alvin69david@gmail.com>
2025-12-17 20:05:07 +0300

    Editted onboarding1

--- events_uganda/lib/Intro/Onboarding_Screen3.dart
+++ events_uganda/lib/Intro/Onboarding_Screen3.dart
@@ -40,7 +40,7 @@ class _OnboardingScreen3State extends State<OnboardingScreen3> {
             ),
           ),
           Positioned(
-            top: screenHeight * 0.098,
+            top: screenHeight * 0.100,
             left: screenWidth * 0.143,
             child: ClipOval(
               child: Image.asset(
