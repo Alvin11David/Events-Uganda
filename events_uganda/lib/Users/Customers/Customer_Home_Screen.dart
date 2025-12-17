e13f9f20ec79ea23ad0e315bc59b9b8bfe7897e1
Alvin11David <alvin69david@gmail.com>
2025-12-17 20:57:38 +0300

    Changed the size

--- events_uganda/lib/Users/Customers/Customer_Home_Screen.dart
+++ events_uganda/lib/Users/Customers/Customer_Home_Screen.dart
@@ -168,8 +168,8 @@ class _CustomerHomeScreenState extends State<CustomerHomeScreen>
         : (index == 2 ? 11 * 3.14159 / 180 : 0.0);
 
     // Adjust these values to move left/right images
-    final offsetX = index == 0 ? -28.0 : (index == 2 ? -28.0 : 0.0);
-    final offsetY = index == 0 ? 29.0 : (index == 2 ? -29.0 : 0.0);
+    final offsetX = index == 0 ? -28.0 : (index == 2 ? 28.0 : 0.0);
+    final offsetY = index == 0 ? 35.0 : (index == 2 ? -1.0 : 0.0);
 
     return AnimatedContainer(
       duration: const Duration(milliseconds: 300),
